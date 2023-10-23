14d0529a6a44abccd8d5815265d91ff30caddee9
z7311873 <33269587+z7311873@users.noreply.github.com>
2023-10-24 01:42:21 +0800

    Update YZM.js

--- YZM.js
+++ YZM.js
@@ -7,20 +7,30 @@ var randomIndex = Math.floor(Math.random() * characters.length);
 code += characters.charAt(randomIndex);
     }
     
-document.getElementById("yzm").innerHTML ="code:  &nbsp&nbsp           "+ code;
+tu=document.getElementById("tu")
+
+
+//console.log(t.toDataURL());
+
+tuz=tu.getContext("2d")
+tuz.font='50px Arial'
+tuz.fillText(code,50,50)
+var image = new Image();
+image.src = tu.toDataURL()
+console.log(image.src)
+document.body.appendChild(image);
+
 }
 huan()
 function yz(){
     var z=document.getElementById("yzz").value
-    console.log(z)
-    console.log(code)
     if(z==code){
         alert("you are right !!")
     }
     else{
         alert("you are error!")
+        document.getElementById("wai").innerHTML="<canvas id='tu'></canvas>"
         huan()
-        document.getElementById("yzz").value=""
     }
 }
   
